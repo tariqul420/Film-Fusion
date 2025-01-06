650fc73abe4f41a84162ae9dffdc50bc51d0ebfb
tariqul420 <tariqul.islam.programmer@gmail.com>
2025-01-06 09:12:34 +0600

    upcomeing done

--- src/Components/Home/Upcoming.jsx
+++ src/Components/Home/Upcoming.jsx
@@ -41,15 +41,15 @@ const Upcoming = ({ movie, setUpcoming, upcoming }) => {
                 alt={movieName}
                 className="w-full h-[60%] object-cover group-hover:opacity-40 group-hover:h-full transition-all duration-300 ease-out" />
 
-            <div className="absolute bottom-0 left-0 py-[20px]  px-[20px] w-full">
-                <h3 className="text-[1.4rem] font-bold uppercase">{movieName}</h3>
+            <div className="absolute bottom-0 left-0 py-[10px]  px-[10px] w-full">
+                <h3 className="text-[1.4rem] font-bold uppercase">{movieName?.length > 16 ? `${movieName.slice(0, 16)}...` : movieName}</h3>
                 <p>{releaseDate}</p>
-                <p>{description}</p>
+                <p>{description?.length > 60 ? `${description.slice(0, 60)}...` : description}</p>
 
                 <div>
                     <button
                         onClick={() => handelUpcomingDelete(_id)}
-                        className="border-2 border-solid border-color-accent px-4 py-2 rounded-full mt-3 font-bold hover:bg-color-accent dark:text-color-text-d transition-all duration-300 ease-out">Delete Upcoming</button>
+                        className="border-2 border-solid border-color-accent px-3 py-1 rounded-full mt-1 font-bold hover:bg-color-accent dark:text-color-text-d transition-all duration-300 ease-out">Delete Upcoming</button>
                 </div>
             </div>
         </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -199,9 +199,9 @@ const Home = () => {
             <div className="w-10/12 mx-auto mb-20">
                 <h2 className="text-6xl font-bold text-center">Upcoming Movies</h2>
                 {
-                    upcoming.length == 0 ? (
+                    upcoming?.length == 0 ? (
                         <div
-                            className="boxShadow p-6 sm:px-20 sm:py-14 flex items-center justify-center flex-col gap-[4px] rounded-xl dark:bg-gray-700 bg-white shadow-md mt-12">
+                            className="p-6 sm:px-20 sm:py-14 flex items-center justify-center flex-col gap-[4px] rounded-xl dark:bg-gray-700 bg-white shadow-md mt-12">
                             <img src="https://i.ibb.co/cgfgxGH/Illustrations.png" alt="empty/image" className="w-full sm:w-[200px]" />
 
                             <h1 className="text-[3rem] mt-6 font-[500]">Result Not Found</h1>
@@ -209,7 +209,7 @@ const Home = () => {
                             <p className="text-[0.9rem] dark:text-gray-300 text-gray-600">Whoops ... this information is not available for a moment</p>
                         </div>
                     ) : (
-                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-20">
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-20">
                             {
                                 upcoming.map(movie => <Upcoming key={movie._id} movie={movie} upcoming={upcoming} setUpcoming={setUpcoming} />)
                             }
